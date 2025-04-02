5621221f5559d8ac1e86369087e53bbde33de286
hutesh <vidhateha22.comp@coep.ac.in>
2025-04-02 20:36:00 +0530

    first

---
+++ 2-4-25/que15/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <h2 className="page about">About Us: We build great apps!</h2>;
+}
+
+export default About;
---
+++ 2-4-25/que15/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contact() {
+  return <h2 className="page contact">Contact Us: Reach us anytime!</h2>;
+}
+
+export default Contact;
